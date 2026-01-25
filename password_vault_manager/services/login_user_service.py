cb199dc53e78f00901f356d7e113c6b19890a825
Shubham Khirwadkar <khirwadkar.shubham25@gmail.com>
2026-01-25 00:36:14 -0600

    Login User

--- password_vault_manager/services/login_user_service.py
+++ password_vault_manager/services/login_user_service.py
@@ -9,20 +9,25 @@ from rest_framework import status
 from password_manager import settings
 from password_manager.commons.generic_constants import GenericConstants
 from password_vault_manager.models import Users
+from password_vault_manager.services.crypto_service import CryptoService
 from password_vault_manager.services.service_helper.password_vault_manager_service_helper import \
     PasswordVaultManagerServiceHelper
 
 
-class LoginService(PasswordVaultManagerServiceHelper):
+class LoginUserService(PasswordVaultManagerServiceHelper):
 
     def __init__(self):
         super().__init__()
 
 
     def get_request_params(self, *args, **kwargs):
+        """Extract and clean request parameters"""
+        username = kwargs.get('data').get('username', '').strip()
+        password = kwargs.get('data').get('password', '')
+
         return {
-            'email': kwargs.get('data').get('email'),
-            'password': kwargs.get('data').get('password')
+            'username': username,
+            'password': password
         }
 
     def get_data(self, *args, **kwargs):
@@ -33,39 +38,38 @@ class LoginService(PasswordVaultManagerServiceHelper):
             return {"message": message}
 
         try:
-            user = Users.objects.get(email=params.get('email'))
+            user = Users.objects.filter(username=params.get("username")).first()
 
-            key = ""
-            with open('secret.key', 'rb') as f:
-                key = f.read().strip()
+            # Check if user exists
+            if not user:
+                self.set_status_code(status_code=status.HTTP_401_UNAUTHORIZED)
+                return {"message": GenericConstants.USER_NOT_FOUND}
 
-            password = Fernet(key).decrypt(user.password.encode()).decode()
-
-            if password != params['password']:
+            is_password_valid = CryptoService.verify_master_password(params.get("password"), user.password)
+            if not is_password_valid:
                 self.set_status_code(status_code=status.HTTP_401_UNAUTHORIZED)
                 return {"message": GenericConstants.INVALID_EMAIL_PASSWORD}
 
-            token_payload = {
-                "id": user.id,
-                "username": user.username,
-                "first_name": user.first_name,
-                "last_name": user.last_name
+            api_payload = {
+                'user_id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
             }
 
-            api_token_payload = copy.deepcopy(token_payload)
-            api_token_payload["exp"] = self.get_expiry(GenericConstants.API_TOKEN_TYPE)
-            api_token = jwt.encode(api_token_payload, settings.SECRET_KEY)
+            # Generate both API and refresh tokens
+            tokens = self.generate_tokens(api_payload)
 
-            refresh_token_payload = copy.deepcopy(token_payload)
-            refresh_token_payload["exp"] = self.get_expiry(GenericConstants.REFRESH_TOKEN_TYPE)
-            refresh_token = jwt.encode(refresh_token_payload, settings.SECRET_KEY)
-
+            self.set_status_code(status_code=status.HTTP_200_OK)
             return {
-                'api_token': api_token,
-                'refresh_token': refresh_token,
-                'user_info': token_payload,
-                'message': GenericConstants.LOGIN_SUCCESS_MESSAGE
+                'message': GenericConstants.LOGIN_SUCCESS_MESSAGE,
+                'user_id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                **tokens
             }
-        except OBJECT_DOES_NOT_EXIST as e:
-            self.set_status_code(status.HTTP_400_BAD_REQUEST)
-            return {"message": GenericConstants.USER_NOT_FOUND}
+
+        except Exception as e:
+            self.set_status_code(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return {
+                'message': GenericConstants.LOGIN_FAILED_MESSAGE
+            }
