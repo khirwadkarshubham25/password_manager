4d20ef7be11e861df135a389b8c76bf583971477
Shubham Khirwadkar <khirwadkar.shubham25@gmail.com>
2026-01-24 22:47:37 -0600

    Urls

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vault/', include("password_vault_manager.urls")),
+    path('', include("password_vault_manager.urls")),
 ]
--- password_vault_manager/urls.py
+++ password_vault_manager/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from password_vault_manager import views
 
 urlpatterns = [
-    path("signup", views.SignUp.as_view(), name='signup'),
-    path("create_master_password", views.CreateMasterPasswordView.as_view(), name='create_master_password'),
-    path("login", views.Login.as_view(), name='login'),
-    path("home", views.Home.as_view(), name='home')
+    path("register/", views.RegisterUser.as_view(), name='register'),
+    path("login/", views.Login.as_view(), name='login'),
+    path("dashboard/", views.Dashboard.as_view(), name='dashboard'),
+    path("manage_password", views.ManagePassword.as_view(), name='manage_password'),
 ]
