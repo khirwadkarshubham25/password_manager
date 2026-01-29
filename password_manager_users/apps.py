7ed7bfe1e119ae927fe7bab1a4c7ce25446386ee
Shubham Khirwadkar <khirwadkar.shubham25@gmail.com>
2026-01-28 22:01:10 -0600

    Clean Up

--- password_manager_users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PasswordManagerUsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'password_manager_users'
--- password_manager_users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from password_manager_users import views
-
-urlpatterns = [
-    path("register", views.RegisterUser.as_view(), name='register'),
-    path("login", views.Login.as_view(), name='login')
-]
